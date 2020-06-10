84b9204c4ec9aa8c306faa28d9e3764871df18ea
JoeBeaulieu <j.beaulieu.apps@gmail.com>
2020-06-09 23:52:43 -0400

    Migrate StethoInitializer to Kotlin

--- app/src/main/java/com/example/rdpocketpal2/application/MyApplication.java
+++ app/src/main/java/com/example/rdpocketpal2/application/MyApplication.java
@@ -1,15 +1,14 @@
 package com.example.rdpocketpal2.application;
 
 import android.app.Application;
-import android.content.pm.ApplicationInfo;
 
-import com.example.rdpocketpal2.util.StethoInitializer;
+import com.example.rdpocketpal2.util.StethoInitializerKotlin;
 
 public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        // initialize stetho
-        new StethoInitializer(this).initialize();
+        // initialize Stetho
+        new StethoInitializerKotlin(this).initialize();
     }
 }
